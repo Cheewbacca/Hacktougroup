826b3eaae896a716f12027b47418c30cfa993add
Cheewbacca <vanya.timoschenko85@gmail.com>
2020-11-26 22:54:50 +0200

    header release

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -3,3 +3,10 @@
 var add = function add(a, b) {
   return a + b;
 };
+
+var burger = $('#burger');
+burger.on('click', function () {
+  burger.toggleClass('closed');
+  $('.header__navigation-opened').toggleClass('hidded');
+  $('.header__navigation-closed').toggleClass('hidded');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 const add = (a, b) => a + b;
+
+var burger = $('#burger');
+
+burger.on('click',  function(){
+    burger.toggleClass('closed');
+    $('.header__navigation-opened').toggleClass('hidded');
+    $('.header__navigation-closed').toggleClass('hidded');
+});
