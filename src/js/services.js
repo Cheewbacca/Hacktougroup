3cd7d6429fc05cadb9826a8f6ea7a089e67af293
Cheewbacca <vanya.timoschenko85@gmail.com>
2020-12-06 15:33:58 +0200

    services page mobile and tablet

--- src/js/services.js
+++ src/js/services.js
@@ -1,18 +1,20 @@
-var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 100,
-
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+if ($(window).width() > 768){
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 100,
+    
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
 
 var switcher = $('#switcher').children();
 
